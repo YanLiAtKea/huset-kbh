d98caed088de4f283a44f73cfa8c38e964aa9254
liyan <yanx0167@stud.kea.dk>
2018-04-28 12:04:51 +0200

    ADDED: huset logo and map at the bottom on index page

--- js/index.js
+++ js/index.js
@@ -160,8 +160,8 @@ let checkBottom = setInterval(function(){
 }, 700)
 function bottomVisible() {
   const scrollY = window.scrollY
-  const visible = document.documentElement.clientHeight
-  const pageHeight = document.documentElement.scrollHeight
+  const visible = document.documentElement.clientHeight;
+  const pageHeight = document.documentElement.scrollHeight;
   const bottomOfPage = visible + scrollY >= pageHeight
   return bottomOfPage || pageHeight < visible
 }
